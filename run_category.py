5bdbdd020a67fce85c4227734e9b9fa83ab25ef3
kaCVanime <kacvanime@gmail.com>
2024-12-19 15:53:06 +0800

    fix; use title as name

--- run_category.py
+++ run_category.py
@@ -43,71 +43,63 @@ class Parser:
             return int(float(r.group(1)))
         return 0
 
+    def _get_full_topic_name(self, el):
+        title = el.get("title", '')
+        s = 'Category:'
+        return title[len(s):] if title.startswith(s) else el.get_text()
+
     def _get_children_names(self, lis):
         results = []
         for li in lis:
             if not (ul := li.ul):
-                results.append(li.get_text())
+                results.append(self._get_full_topic_name(li.a))
             else:
                 t = li.find('a', recursive=False)
-                name = t.get_text() if t else ''
+                name = self._get_full_topic_name(t)
                 results.append(f'{name}({"·".join([a.get_text() for a in ul.find_all("a")])})')
         return results
 
-    def _parse_h(self, h, level):
+    def _parse_h(self, h):
         uls = h.find_all('ul', recursive=False)
         assert len(uls) == 1, NotImplementedError
         lis = uls[0].find_all('li', recursive=False)
         b = lis[0].b
         header = lis[0].get_text() if b else None
+        if lis[0].get_text().startswith('See also'):
+            return []
         if header:
-            if header.startswith('See also'):
-                return None
             return {
                 "subject": header,
-                "level": level,
+                "level": self._get_hs_level(h) + 2,
                 "children": self._get_children_names(lis[1:]),
                 "subcategory": []
             }
         return self._get_children_names(lis)
 
-    def _parse(self, hs, i, level):
-        results = []
-        while i < len(hs):
-            h = hs[i]
-            cat = self._parse_h(h, level)
+    def parse_content(self, content):
+        hs = content.find_all('div', class_='hlist')
+        tree = []
+        stack = []
+        for h in hs:
+            cat = self._parse_h(h)
             if cat is None:
-                i = i + 1
                 continue
-            elif type(cat) is list:
-                results.extend(cat)
-                i = i + 1
+            if type(cat) is list:
+                t = tree if not stack else stack[-1]["subcategory"]
+                t.extend(cat)
                 continue
             else:
-                results.append(cat)
+                while stack and stack[-1]["level"] >= cat["level"]:
+                    stack.pop()
 
-                c_level = self._get_hs_level(h)
-                p = i
-                while (p := p+1) < len(hs):
-                    next_h_level = self._get_hs_level(hs[p])
-                    if next_h_level == c_level + 1:
-                        cat["subcategory"].extend(self._parse(hs, p, level+1))
-                        continue
-                    if next_h_level <= c_level:
-                        if c_level == 0:
-                            break
-                        return results
-                    if next_h_level > c_level + 1:
-                        continue
+                if stack:
+                    stack[-1]["subcategory"].append(cat)
+                else:
+                    tree.append(cat)
 
-                i = p
+                stack.append(cat)
 
-        return results
-
-
-    def parse_content(self, content, level):
-        hs = content.find_all('div', class_='hlist')
-        return self._parse(hs, 0, level)
+        return tree
 
 
     def format(self, tree):
@@ -155,7 +147,7 @@ class Parser:
                 {
                     "subject": self.get_entry_name(e),
                     "level": 1,
-                    "children": self.parse_content(self.get_contents(e), 2)
+                    "children": self.parse_content(self.get_contents(e))
                 } for e in self.get_entries()
             ], ft
         )
